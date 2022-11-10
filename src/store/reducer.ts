17548fa411b29e4209709f857e0102d7c00953cc
Aleksandr Kitov <kitov.nn@gmail.com>
2022-11-10 11:00:30 +0100

    feat(state): add sample state

--- src/store/reducer.ts
+++ src/store/reducer.ts
@@ -6,7 +6,23 @@ export type AnyAction = {
 };
 
 export const initialState: ApplicationState = {
-  columns: [],
+  columns: [
+    {
+      id: '1',
+      title: 'To do',
+      cards: [
+        { id: '1', text: 'implement basic state management' },
+        { id: '2', text: 'create base components structure' },
+        { id: '3', text: '??' },
+        { id: '4', text: 'profit' },
+      ],
+    },
+    {
+      id: '2',
+      title: 'In progress',
+      cards: [],
+    }
+  ],
 };
 
 export function reducer(state: ApplicationState, action: AnyAction) {
